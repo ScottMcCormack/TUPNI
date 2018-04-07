2498981cbf18b7b3f29caad125c7247b4eb314c5
Scott McCormack <scott.mccormack@outlook.com>
2018-04-07 18:41:40 +0800

    Use schema to dictate sample file size, added parser to process bytearray input

--- examples/section3_example.py
+++ examples/section3_example.py
@@ -1,20 +1,26 @@
 # The following is a replica of the example mentioned in Section3
 
 
-def build_sample_record(records, byteorder):
+def build_sample_record(records, schema, byteorder):
     """Build a byte record that is similar to that presented in Figure 2
 
     The record data is just a sequence of null bytes
 
     :param list records: A list of (record type, record size) tuples
+    :param dict schema: A dict of file schema byte sizes
     :param str byteorder: The way that the
     :return:
     """
     sample_input = bytearray()
 
+    # Read the schema information for building the file
+    header_byte_size = schema['header_byte_size']
+    record_type_byte_size = schema['record_type_byte_size']
+    record_size_byte_size = schema['record_size_byte_size']
+
     # Build the header, as a 4 byte representation
     num_records = len(records)
-    file_header = num_records.to_bytes(4, byteorder=byteorder)
+    file_header = num_records.to_bytes(header_byte_size, byteorder=byteorder)
     sample_input.extend(file_header)
 
     # Build out the two records
@@ -22,11 +28,11 @@ def build_sample_record(records, byteorder):
         rt, rs = record
 
         # Record type, 2 byte representation
-        rt_bytes = rt.to_bytes(2, byteorder=byteorder)
+        rt_bytes = rt.to_bytes(record_type_byte_size, byteorder=byteorder)
         sample_input.extend(rt_bytes)
 
         # Record size, 2 byte representation
-        rs_bytes = rs.to_bytes(2, byteorder=byteorder)
+        rs_bytes = rs.to_bytes(record_size_byte_size, byteorder=byteorder)
         sample_input.extend(rs_bytes)
 
         # Now add in records data as an empty byte representation
@@ -36,19 +42,60 @@ def build_sample_record(records, byteorder):
     return sample_input
 
 
-def parse_sample_record(input):
-    pass
+def parse_sample_record(input, schema, byteorder):
+    """Parse a bytearray using a fileschema
+
+    :param bytearray input:
+    :param dict schema: A dict of file schema byte sizes
+    :return:
+    """
+
+    input_len = len(input)
+
+    # Read the schema information for parsing the file
+    header_byte_size = schema['header_byte_size']
+    record_type_byte_size = schema['record_type_byte_size']
+    record_size_byte_size = schema['record_size_byte_size']
+
+    # Get the number of records in the input
+    i = header_byte_size
+    num_records = int.from_bytes(input[0:i], byteorder=byteorder)
+
+    while i < input_len:
+        # Read the record type
+        record_type_bytes = input[i:i + record_type_byte_size]
+        record_type = int.from_bytes(record_type_bytes, byteorder=byteorder)
+        print(record_type)
+
+        i += record_type_byte_size
+
+        # Read the record size
+        record_size_bytes = input[i: i + record_size_byte_size]
+        record_size = int.from_bytes(record_size_bytes, byteorder=byteorder)
+        print(record_size)
+
+        i += (record_size - record_type_byte_size)
 
 
 def main():
     # Get the sample input
 
+    # Schema for the file input
+    schema = {
+        "header_byte_size": 4,
+        "record_type_byte_size": 2,
+        "record_size_byte_size": 2
+    }
+    byteorder = 'big'
+
     # records consists of tuples represented as:
     # 1.  Record type
     # 2.  Record length
     records = [(1, 13), (5, 10)]
-    sample_input = build_sample_record(records, 'big')
-    print(sample_input)
+    sample_input = build_sample_record(records, schema, byteorder)
+
+    # Parse the sample file
+    parse_sample_record(sample_input, schema, byteorder)
 
 
 if __name__ == "__main__":
